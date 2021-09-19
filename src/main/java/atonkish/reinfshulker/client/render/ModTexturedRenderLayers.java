292f75d47c530b0cb019b1bd96aef96221a9cc9e
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-09-19 15:00:15 +0900

    feat: add client codes

---
+++ src/main/java/atonkish/reinfshulker/client/render/ModTexturedRenderLayers.java
@@ -0,0 +1,61 @@
+package atonkish.reinfshulker.client.render;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Identifier;
+
+import atonkish.reinfcore.util.ReinforcingMaterial;
+import atonkish.reinfshulker.ReinforcedShulkerBoxesMod;
+
+@Environment(EnvType.CLIENT)
+public class ModTexturedRenderLayers {
+    public static final Identifier SHULKER_BOXES_ATLAS_TEXTURE = new Identifier("textures/atlas/shulker_boxes.png");
+    public static final HashMap<ReinforcingMaterial, Identifier> REINFORCED_SHULKER_BOXES_ATLAS_TEXTURE_MAP;
+    private static final HashMap<ReinforcingMaterial, RenderLayer> REINFORCED_SHULKER_BOXES_RENDER_LAYER_MAP;
+    public static final HashMap<ReinforcingMaterial, SpriteIdentifier> REINFORCED_SHULKER_TEXTURE_ID_MAP;
+    public static final HashMap<ReinforcingMaterial, List<SpriteIdentifier>> COLORED_REINFORCED_SHULKER_BOXES_TEXTURES_MAP;
+
+    public static RenderLayer getReinforcedShulkerBoxes(ReinforcingMaterial material) {
+        return REINFORCED_SHULKER_BOXES_RENDER_LAYER_MAP.get(material);
+    }
+
+    static {
+        REINFORCED_SHULKER_BOXES_ATLAS_TEXTURE_MAP = new HashMap<>();
+        REINFORCED_SHULKER_BOXES_RENDER_LAYER_MAP = new HashMap<>();
+        REINFORCED_SHULKER_TEXTURE_ID_MAP = new HashMap<>();
+        COLORED_REINFORCED_SHULKER_BOXES_TEXTURES_MAP = new HashMap<>();
+        for (ReinforcingMaterial material : ReinforcingMaterial.values()) {
+            // Atlas Texture
+            Identifier atlasTexture = new Identifier(ReinforcedShulkerBoxesMod.MOD_ID,
+                    "textures/atlas/" + material + "_shulker_boxes.png");
+            REINFORCED_SHULKER_BOXES_ATLAS_TEXTURE_MAP.put(material, atlasTexture);
+
+            // Render Layer
+            RenderLayer renderLayer = RenderLayer.getEntityCutout(atlasTexture);
+            REINFORCED_SHULKER_BOXES_RENDER_LAYER_MAP.put(material, renderLayer);
+
+            // Sprite Identifiers
+            // Non-Color
+            SpriteIdentifier nonColoredTextureId = new SpriteIdentifier(
+                    REINFORCED_SHULKER_BOXES_ATLAS_TEXTURE_MAP.get(material), new Identifier(
+                            ReinforcedShulkerBoxesMod.MOD_ID, "entity/reinforced_shulker/" + material + "/shulker"));
+            REINFORCED_SHULKER_TEXTURE_ID_MAP.put(material, nonColoredTextureId);
+            // Color
+            List<SpriteIdentifier> coloredTextureIds = Stream.of(DyeColor.values()).map((color) -> {
+                return new SpriteIdentifier(REINFORCED_SHULKER_BOXES_ATLAS_TEXTURE_MAP.get(material),
+                        new Identifier(ReinforcedShulkerBoxesMod.MOD_ID,
+                                "entity/reinforced_shulker/" + material + "/shulker_" + color));
+            }).collect(ImmutableList.toImmutableList());
+            COLORED_REINFORCED_SHULKER_BOXES_TEXTURES_MAP.put(material, coloredTextureIds);
+        }
+    }
+}
---
+++ src/main/java/atonkish/reinfshulker/client/render/block/entity/ReinforcedShulkerBoxBlockEntityRenderer.java
@@ -0,0 +1,68 @@
+package atonkish.reinfshulker.client.render.block.entity;
+
+import atonkish.reinfcore.util.ReinforcingMaterial;
+import atonkish.reinfshulker.block.entity.ReinforcedShulkerBoxBlockEntity;
+import atonkish.reinfshulker.client.render.ModTexturedRenderLayers;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.ShulkerEntityModel;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.math.Direction;
+
+@Environment(EnvType.CLIENT)
+public class ReinforcedShulkerBoxBlockEntityRenderer implements BlockEntityRenderer<ReinforcedShulkerBoxBlockEntity> {
+    private final ShulkerEntityModel<?> model;
+
+    public ReinforcedShulkerBoxBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
+        this.model = new ShulkerEntityModel<>(ctx.getLayerModelPart(EntityModelLayers.SHULKER));
+    }
+
+    public void render(ReinforcedShulkerBoxBlockEntity reinforcedShulkerBoxBlockEntity, float tickDelta,
+            MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        Direction direction = Direction.UP;
+        if (reinforcedShulkerBoxBlockEntity.hasWorld()) {
+            BlockState blockState = reinforcedShulkerBoxBlockEntity.getWorld()
+                    .getBlockState(reinforcedShulkerBoxBlockEntity.getPos());
+            if (blockState.getBlock() instanceof ShulkerBoxBlock) {
+                direction = (Direction) blockState.get(ShulkerBoxBlock.FACING);
+            }
+        }
+
+        DyeColor color = reinforcedShulkerBoxBlockEntity.getColor();
+        ReinforcingMaterial material = reinforcedShulkerBoxBlockEntity.getMaterial();
+        SpriteIdentifier spriteIdentifier2;
+        if (color == null) {
+            spriteIdentifier2 = ModTexturedRenderLayers.REINFORCED_SHULKER_TEXTURE_ID_MAP.get(material);
+        } else {
+            spriteIdentifier2 = ModTexturedRenderLayers.COLORED_REINFORCED_SHULKER_BOXES_TEXTURES_MAP.get(material)
+                    .get(color.getId());
+        }
+
+        matrices.push();
+        matrices.translate(0.5D, 0.5D, 0.5D);
+        float g = 0.9995F;
+        matrices.scale(g, g, g);
+        matrices.multiply(direction.getRotationQuaternion());
+        matrices.scale(1.0F, -1.0F, -1.0F);
+        matrices.translate(0.0D, -1.0D, 0.0D);
+        ModelPart modelPart = this.model.getLid();
+        modelPart.setPivot(0.0F, 24.0F - reinforcedShulkerBoxBlockEntity.getAnimationProgress(tickDelta) * 0.5F * 16.0F,
+                0.0F);
+        modelPart.yaw = 270.0F * reinforcedShulkerBoxBlockEntity.getAnimationProgress(tickDelta) * 0.017453292F;
+        VertexConsumer vertexConsumer = spriteIdentifier2.getVertexConsumer(vertexConsumers,
+                RenderLayer::getEntityCutoutNoCull);
+        this.model.render(matrices, vertexConsumer, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+        matrices.pop();
+    }
+}
