baac18404e96205f2c83d87ddf6787f65d99dafd
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-09-19 10:34:08 +0900

    feat: add block entity mixin

---
+++ src/main/java/atonkish/reinfshulker/block/entity/BlockEntityInterface.java
@@ -0,0 +1,7 @@
+package atonkish.reinfshulker.block.entity;
+
+import net.minecraft.block.entity.BlockEntityType;
+
+public interface BlockEntityInterface {
+    public void setType(BlockEntityType<?> type);
+}
