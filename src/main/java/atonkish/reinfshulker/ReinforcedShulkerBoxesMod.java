7a73f3591dddf708defaf7370b2d75d122121400
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-01-24 22:58:11 +0900

    refactor: change logger

--- src/main/java/atonkish/reinfshulker/ReinforcedShulkerBoxesMod.java
+++ src/main/java/atonkish/reinfshulker/ReinforcedShulkerBoxesMod.java
@@ -3,8 +3,8 @@ package atonkish.reinfshulker;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.DyeColor;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import atonkish.reinfcore.api.ReinforcedCoreModInitializer;
 import atonkish.reinfcore.api.ReinforcedCoreRegistry;
@@ -18,7 +18,7 @@ import atonkish.reinfshulker.util.ReinforcingMaterialSettings;
 
 public class ReinforcedShulkerBoxesMod implements ReinforcedCoreModInitializer {
 	public static final String MOD_ID = "reinfshulker";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitializeReinforcedCore() {
