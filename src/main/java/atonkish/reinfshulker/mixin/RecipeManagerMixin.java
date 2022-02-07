07f7c9c5247338d24c276a230eb951c9bfb25d1f
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-02-07 22:29:16 +0900

    bump version from 2.0.0-beta.1+1.18 to 2.0.0-beta.2+1.18 (#28)
    * chore: bump fabric versions
    
    * chore: remove common tags
    
    * fix: fix advancements and recipes
    
    * feat: integrate with Reinforced Chests
    
    * feat: add reinfchest loaded flag
    
    * feat: remove missing resource identifiers dynamically
    
    * chore: bump version from 2.0.0-beta.1+1.18 to 2.0.0-beta.2+1.18

---
+++ src/main/java/atonkish/reinfshulker/mixin/RecipeManagerMixin.java
@@ -0,0 +1,42 @@
+package atonkish.reinfshulker.mixin;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.google.gson.JsonElement;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.profiler.Profiler;
+
+import atonkish.reinfshulker.ReinforcedShulkerBoxesMod;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+    @Inject(method = "apply", at = @At("HEAD"))
+    private void removeMissingIdentifier(Map<Identifier, JsonElement> map, ResourceManager resourceManager,
+            Profiler profiler, CallbackInfo info) {
+        if (ReinforcedShulkerBoxesMod.IS_REINFCHEST_LOADED) {
+            return;
+        }
+
+        ArrayList<Identifier> missingIdentifiers = new ArrayList<>();
+        for (Identifier id : map.keySet()) {
+            if (!id.getNamespace().equals(ReinforcedShulkerBoxesMod.MOD_ID)) {
+                continue;
+            }
+
+            if (id.getPath().contains("chest")) {
+                missingIdentifiers.add(id);
+            }
+        }
+
+        missingIdentifiers.forEach(map::remove);
+    }
+}
