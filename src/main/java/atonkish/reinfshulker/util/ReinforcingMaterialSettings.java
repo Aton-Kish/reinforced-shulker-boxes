3c70740c3cf8b50fca12e4b979836756ea0b4742
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-12-18 17:22:37 +0900

    fix registry

--- src/main/java/atonkish/reinfshulker/util/ReinforcingMaterialSettings.java
+++ src/main/java/atonkish/reinfshulker/util/ReinforcingMaterialSettings.java
@@ -12,25 +12,24 @@ import net.minecraft.item.Items;
 import net.minecraft.sound.BlockSoundGroup;
 
 import atonkish.reinfcore.api.ReinforcedCoreRegistry;
-import atonkish.reinfcore.item.ModItemGroup;
 import atonkish.reinfcore.util.ReinforcingMaterial;
 
 public enum ReinforcingMaterialSettings {
     COPPER(ReinforcedCoreRegistry.registerReinforcingMaterial("copper", 45, Items.COPPER_INGOT),
             FabricBlockSettings.of(Material.SHULKER_BOX).strength(2.0F, 6.0F).sounds(BlockSoundGroup.COPPER),
-            new Item.Settings().maxCount(1).group(ModItemGroup.REINFORCED_STORAGE)),
+            new Item.Settings().maxCount(1)),
     IRON(ReinforcedCoreRegistry.registerReinforcingMaterial("iron", 54, Items.IRON_INGOT),
             FabricBlockSettings.of(Material.SHULKER_BOX).strength(2.0F, 6.0F).sounds(BlockSoundGroup.METAL),
-            new Item.Settings().maxCount(1).group(ModItemGroup.REINFORCED_STORAGE)),
+            new Item.Settings().maxCount(1)),
     GOLD(ReinforcedCoreRegistry.registerReinforcingMaterial("gold", 81, Items.GOLD_INGOT),
             FabricBlockSettings.of(Material.SHULKER_BOX).strength(2.0F, 6.0F).sounds(BlockSoundGroup.METAL),
-            new Item.Settings().maxCount(1).group(ModItemGroup.REINFORCED_STORAGE)),
+            new Item.Settings().maxCount(1)),
     DIAMOND(ReinforcedCoreRegistry.registerReinforcingMaterial("diamond", 108, Items.DIAMOND),
             FabricBlockSettings.of(Material.SHULKER_BOX).strength(2.0F, 6.0F).sounds(BlockSoundGroup.METAL),
-            new Item.Settings().maxCount(1).group(ModItemGroup.REINFORCED_STORAGE)),
+            new Item.Settings().maxCount(1)),
     NETHERITE(ReinforcedCoreRegistry.registerReinforcingMaterial("netherite", 108, Items.NETHERITE_INGOT),
             FabricBlockSettings.of(Material.SHULKER_BOX).strength(2.0F, 1200.0F).sounds(BlockSoundGroup.NETHERITE),
-            new Item.Settings().maxCount(1).group(ModItemGroup.REINFORCED_STORAGE).fireproof());
+            new Item.Settings().maxCount(1).fireproof());
 
     private final ReinforcingMaterial material;
     private final Block.Settings blockSettings;
