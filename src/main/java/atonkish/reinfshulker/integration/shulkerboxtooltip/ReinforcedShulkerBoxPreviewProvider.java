4fceb871f211e07dd8ca98c8c99d19bdd8b0afdd
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2023-03-21 10:23:02 +0900

    fix shulker box tooltip integ

--- src/main/java/atonkish/reinfshulker/integration/shulkerboxtooltip/ReinforcedShulkerBoxPreviewProvider.java
+++ src/main/java/atonkish/reinfshulker/integration/shulkerboxtooltip/ReinforcedShulkerBoxPreviewProvider.java
@@ -1,7 +1,11 @@
 package atonkish.reinfshulker.integration.shulkerboxtooltip;
 
 import com.misterpemodder.shulkerboxtooltip.api.PreviewContext;
+import com.misterpemodder.shulkerboxtooltip.api.color.ColorKey;
 import com.misterpemodder.shulkerboxtooltip.api.provider.BlockEntityPreviewProvider;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
 import net.minecraft.block.Block;
 import net.minecraft.util.DyeColor;
@@ -10,7 +14,6 @@ import atonkish.reinfcore.util.ReinforcingMaterial;
 import atonkish.reinfshulker.block.ReinforcedShulkerBoxBlock;
 
 public class ReinforcedShulkerBoxPreviewProvider extends BlockEntityPreviewProvider {
-    private static float[] SHULKER_BOX_COLOR = new float[] { 0.592f, 0.403f, 0.592f };
     protected final int maxRowSize;
     private final ReinforcingMaterial material;
 
@@ -29,15 +32,30 @@ public class ReinforcedShulkerBoxPreviewProvider extends BlockEntityPreviewProvi
     }
 
     @Override
-    public float[] getWindowColor(PreviewContext context) {
+    @Environment(EnvType.CLIENT)
+    public ColorKey getWindowColorKey(PreviewContext context) {
         DyeColor dye = ((ReinforcedShulkerBoxBlock) Block.getBlockFromItem(context.stack().getItem())).getColor();
-        if (dye != null) {
-            float[] components = dye.getColorComponents();
-            return new float[] { Math.max(0.15f, components[0]), Math.max(0.15f, components[1]),
-                    Math.max(0.15f, components[2]) };
-        } else {
-            return SHULKER_BOX_COLOR;
-        }
+
+        if (dye == null)
+            return ColorKey.SHULKER_BOX;
+        return switch (dye) {
+            case ORANGE -> ColorKey.ORANGE_SHULKER_BOX;
+            case MAGENTA -> ColorKey.MAGENTA_SHULKER_BOX;
+            case LIGHT_BLUE -> ColorKey.LIGHT_BLUE_SHULKER_BOX;
+            case YELLOW -> ColorKey.YELLOW_SHULKER_BOX;
+            case LIME -> ColorKey.LIME_SHULKER_BOX;
+            case PINK -> ColorKey.PINK_SHULKER_BOX;
+            case GRAY -> ColorKey.GRAY_SHULKER_BOX;
+            case LIGHT_GRAY -> ColorKey.LIGHT_GRAY_SHULKER_BOX;
+            case CYAN -> ColorKey.CYAN_SHULKER_BOX;
+            case PURPLE -> ColorKey.PURPLE_SHULKER_BOX;
+            case BLUE -> ColorKey.BLUE_SHULKER_BOX;
+            case BROWN -> ColorKey.BROWN_SHULKER_BOX;
+            case GREEN -> ColorKey.GREEN_SHULKER_BOX;
+            case RED -> ColorKey.RED_SHULKER_BOX;
+            case BLACK -> ColorKey.BLACK_SHULKER_BOX;
+            default -> ColorKey.WHITE_SHULKER_BOX;
+        };
     }
 
     @Override
