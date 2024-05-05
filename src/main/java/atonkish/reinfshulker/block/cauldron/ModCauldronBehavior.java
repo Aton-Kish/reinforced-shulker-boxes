76d7ffc333a72cb98a1b4df414aa921dfc84bfde
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-05-05 15:32:57 +0900

    fixed cauldron behavior

--- src/main/java/atonkish/reinfshulker/block/cauldron/ModCauldronBehavior.java
+++ src/main/java/atonkish/reinfshulker/block/cauldron/ModCauldronBehavior.java
@@ -1,17 +1,13 @@
 package atonkish.reinfshulker.block.cauldron;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-
 import java.util.Map;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.DyeColor;
-import net.minecraft.util.Util;
+import net.minecraft.util.ItemActionResult;
 
 import atonkish.reinfcore.util.ReinforcingMaterial;
 import atonkish.reinfshulker.block.ModBlocks;
@@ -20,48 +16,33 @@ import atonkish.reinfshulker.item.ModItems;
 import atonkish.reinfshulker.stat.ModStats;
 
 public class ModCauldronBehavior {
-    public static final Map<Item, CauldronBehavior> CLEAN_REINFORCED_SHULKER_BOX_MAP;
     private static final CauldronBehavior CLEAN_REINFORCED_SHULKER_BOX;
 
     public static void init() {
-    }
-
-    static Object2ObjectOpenHashMap<Item, CauldronBehavior> createMap() {
-        return Util.make(new Object2ObjectOpenHashMap<>(), (map) -> {
-            map.defaultReturnValue((state, world, pos, player, hand, stack) -> {
-                return ActionResult.PASS;
-            });
-        });
+        Map<Item, CauldronBehavior> map = CauldronBehavior.WATER_CAULDRON_BEHAVIOR.map();
+        for (Map<DyeColor, Item> materialShulkerBoxMap : ModItems.REINFORCED_SHULKER_BOX_MAP.values()) {
+            for (DyeColor color : DyeColor.values()) {
+                map.put(materialShulkerBoxMap.get(color), CLEAN_REINFORCED_SHULKER_BOX);
+            }
+        }
     }
 
     static {
         CLEAN_REINFORCED_SHULKER_BOX = (state, world, pos, player, hand, stack) -> {
             Block block = Block.getBlockFromItem(stack.getItem());
             if (!(block instanceof ReinforcedShulkerBoxBlock)) {
-                return ActionResult.PASS;
+                return ItemActionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
             } else {
                 if (!world.isClient) {
                     ReinforcingMaterial material = ((ReinforcedShulkerBoxBlock) block).getMaterial();
-                    ItemStack itemStack = new ItemStack(
-                            ModBlocks.REINFORCED_SHULKER_BOX_MAP.get(material).get((DyeColor) null));
-                    if (stack.hasNbt()) {
-                        itemStack.setNbt(stack.getNbt().copy());
-                    }
-
-                    player.setStackInHand(hand, itemStack);
+                    player.setStackInHand(hand, stack.copyComponentsToNewStack(
+                            ModBlocks.REINFORCED_SHULKER_BOX_MAP.get(material).get((DyeColor) null), 1));
                     player.incrementStat(ModStats.CLEAN_REINFORCED_SHULKER_BOX_MAP.get(material));
                     LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
                 }
 
-                return ActionResult.success(world.isClient);
+                return ItemActionResult.success(world.isClient);
             }
         };
-
-        CLEAN_REINFORCED_SHULKER_BOX_MAP = createMap();
-        for (Map<DyeColor, Item> materialShulkerBoxMap : ModItems.REINFORCED_SHULKER_BOX_MAP.values()) {
-            for (DyeColor color : DyeColor.values()) {
-                CLEAN_REINFORCED_SHULKER_BOX_MAP.put(materialShulkerBoxMap.get(color), CLEAN_REINFORCED_SHULKER_BOX);
-            }
-        }
     }
 }
