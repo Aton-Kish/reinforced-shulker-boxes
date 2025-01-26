39b316c4fb33d9fba40cac3e44c474e30b2933a4
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2025-01-26 16:43:51 +0900

    moved texture files

--- src/client/java/atonkish/reinfshulker/client/render/ModTexturedRenderLayers.java
+++ src/client/java/atonkish/reinfshulker/client/render/ModTexturedRenderLayers.java
@@ -25,7 +25,7 @@ public class ModTexturedRenderLayers {
     public static SpriteIdentifier registerMaterialDefaultSprite(String namespace, ReinforcingMaterial material) {
         if (!REINFORCED_SHULKER_TEXTURE_ID_MAP.containsKey(material)) {
             SpriteIdentifier identifier = new SpriteIdentifier(TexturedRenderLayers.SHULKER_BOXES_ATLAS_TEXTURE,
-                    Identifier.of(namespace, "entity/reinforced_shulker/" + material.getName() + "/shulker"));
+                    Identifier.of(namespace, String.format("entity/shulker/%s/shulker", material.getName())));
             REINFORCED_SHULKER_TEXTURE_ID_MAP.put(material, identifier);
         }
 
@@ -38,7 +38,7 @@ public class ModTexturedRenderLayers {
             List<SpriteIdentifier> identifiers = Stream.of(DyeColor.values()).map((color) -> {
                 return new SpriteIdentifier(TexturedRenderLayers.SHULKER_BOXES_ATLAS_TEXTURE,
                         Identifier.of(namespace,
-                                "entity/reinforced_shulker/" + material.getName() + "/shulker_" + color.getName()));
+                                String.format("entity/shulker/%s/shulker_%s", material.getName(), color.getName())));
             }).collect(ImmutableList.toImmutableList());
             COLORED_REINFORCED_SHULKER_BOXES_TEXTURES_MAP.put(material, identifiers);
         }
